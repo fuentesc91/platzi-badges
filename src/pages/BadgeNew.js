5d92af8f6e8c755567d73576d3bced5a65547dbf
Carlos Fuentes <fuentesc91@gmail.com>
2019-11-22 01:05:21 -0600

    Migration to actual react project

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
+
+import header from "../images/badge-header.svg";
+
+import "./styles/BadgeNew.css";
+
+class BadgeNew extends React.Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                avatarURL="https://www.gravatar.com/avatar"
+                email={this.state.form.email}
+                jobTitle={this.state.form.jobTitle}
+                twitter={this.state.form.twitter}
+              />
+            </div>
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
